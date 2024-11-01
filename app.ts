7180a4c275d39ce9fba7def5c7c3a7967fb9f25c
Diego Rocha <drocha@griddynamics.com>
2024-11-01 15:00:17 -0600

    Adding types to classes

--- app.ts
+++ app.ts
@@ -1,4 +1,21 @@
-class Observer {
+interface IObserverHandlers {
+  next?: (value: any) => void;
+  error?: (error: any) => void;
+  complete?: () => void;
+}
+
+interface IObserver {
+  next: (value: any) => void;
+  error: (error: any) => void;
+  complete: () => void;
+  unsubscribe: () => void;
+}
+
+class Observer implements IObserver {
+  private handlers: IObserverHandlers;
+  private isUnsubscribed: boolean;
+  private _unsubscribe?: () => void;
+
   constructor(handlers) {
     this.handlers = handlers;
     this.isUnsubscribed = false;
@@ -37,29 +54,40 @@ class Observer {
       this._unsubscribe();
     }
   }
+
+  setUnsubscribe(unsubscribeFn?: () => void) {
+    this._unsubscribe = unsubscribeFn;
+  }
 }
 
-class Observable {
-  constructor(subscribe) {
+interface IObservable {
+  _subscribe: (observer: Observer) => (() => void) | void;
+}
+
+class Observable<T> implements IObservable {
+  public _subscribe: (observer: Observer) => (() => void) | void;
+
+  constructor(subscribe: (observer: Observer) => (() => void) | void) {
     this._subscribe = subscribe;
   }
 
-  static from(values) {
+  static from<T>(values: T[]): Observable<T> {
     return new Observable((observer) => {
       values.forEach((value) => observer.next(value));
-
       observer.complete();
-
       return () => {
         console.log("unsubscribed");
       };
     });
   }
 
-  subscribe(obs) {
+  subscribe(obs: Partial<IObserver>) {
     const observer = new Observer(obs);
 
-    observer._unsubscribe = this._subscribe(observer);
+    const unsubscribeFn = this._subscribe(observer);
+    observer.setUnsubscribe(
+      typeof unsubscribeFn === "function" ? unsubscribeFn : undefined
+    );
 
     return {
       unsubscribe() {
@@ -69,13 +97,21 @@ class Observable {
   }
 }
 
-const HTTP_POST_METHOD = "POST";
-const HTTP_GET_METHOD = "GET";
+const HTTP_POST_METHOD: "POST" = "POST";
+const HTTP_GET_METHOD: "GET" = "GET";
 
-const HTTP_STATUS_OK = 200;
-const HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
+const HTTP_STATUS_OK: 200 = 200;
+const HTTP_STATUS_INTERNAL_SERVER_ERROR: 500 = 500;
 
-const userMock = {
+type UserMockType = {
+  name: string;
+  age: number;
+  roles: string[];
+  createdAt: Date;
+  isDeleated: boolean;
+};
+
+const userMock: UserMockType = {
   name: "User Name",
   age: 26,
   roles: ["user", "admin"],
@@ -83,7 +119,15 @@ const userMock = {
   isDeleated: false,
 };
 
-const requestsMock = [
+type requestMockType = {
+  method: typeof HTTP_POST_METHOD | typeof HTTP_GET_METHOD;
+  host: string;
+  path: string;
+  body?: UserMockType;
+  params: Record<string, any>; // { [prop: string]: any }
+};
+
+const requestsMock: requestMockType[] = [
   {
     method: HTTP_POST_METHOD,
     host: "service.example",
@@ -101,7 +145,11 @@ const requestsMock = [
   },
 ];
 
-const handleRequest = (request) => {
+type HandleRequestType = (request: any) => {
+  status: typeof HTTP_STATUS_OK | typeof HTTP_STATUS_INTERNAL_SERVER_ERROR;
+};
+
+const handleRequest: HandleRequestType = (request) => {
   // handling of request
   return { status: HTTP_STATUS_OK };
 };
